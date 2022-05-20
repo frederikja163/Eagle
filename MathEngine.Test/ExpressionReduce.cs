a9b5d9add0f4cdb097f06d4664832a919f5f43ee
FrederikJA <fjaplv64@gmail.com>
2022-05-20 06:57:51 +0200

    simpler unit tests

--- MathEngine.Test/ExpressionReduce.cs
+++ MathEngine.Test/ExpressionReduce.cs
@@ -6,15 +6,27 @@ namespace MathEngine.Test
 {
     public class ExpressionReduce
     {
-        [TestCase("1+2+3+4+5", 15)]
-        [TestCase("1+2+3+4-5", 5)]
-        [TestCase("-5+4+3+2+1", 5)]
-        [TestCase("-5-4-3-2-1", -15)]
-        public void SummationParse(string equation, int result)
+        private static void TestEquality(string equation, int result)
         {
             IExpression expression = IExpression.Parse(equation).Reduce();
             Assert.IsInstanceOf<Scalar>(expression);
             Assert.AreEqual(result, ((Scalar)expression).Value);
+        }
+        
+        [TestCase("1", 1)]
+        [TestCase("-10", -10)]
+        public void ScalarReduce(string equation, int result)
+        {
+            TestEquality(equation, result);
+        }
+        
+        [TestCase("1+2+3+4+5", 15)]
+        [TestCase("1+2+3+4-5", 5)]
+        [TestCase("-5+4+3+2+1", 5)]
+        [TestCase("-5-4-3-2-1", -15)]
+        public void SummationReduce(string equation, int result)
+        {
+            TestEquality(equation, result);
         }
     }
 }
